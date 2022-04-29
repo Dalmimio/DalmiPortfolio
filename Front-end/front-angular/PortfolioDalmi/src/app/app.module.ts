949f20e6d445e1f5e309c84e0b62c18d6cc6ed5e
Dalila <ilikeitati@gmail.com>
2022-04-29 00:16:19 -0300

    Agregando los componentes correspondientes

--- Front-end/front-angular/PortfolioDalmi/src/app/app.module.ts
+++ Front-end/front-angular/PortfolioDalmi/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AboutComponent,
+    ContactoComponent,
+    FooterComponent,
+    FormacionComponent,
+    HeaderComponent,
+    PortfolioComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front-end/front-angular/PortfolioDalmi/src/app/modelname.model.spec.ts
@@ -0,0 +1,7 @@
+import { Modelname } from './modelname.model';
+
+describe('Modelname', () => {
+  it('should create an instance', () => {
+    expect(new Modelname()).toBeTruthy();
+  });
+});
